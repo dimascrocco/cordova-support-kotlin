e5be7bb5ccb4518102f87558a96669ed5ee6d086
Dimas Oliveira <dimas@phelcom.com.br>
2018-05-28 18:41:04 -0300

    change kotlin version and gradleFilename path

--- src/support-kotlin.js
+++ src/support-kotlin.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-const gradleFilename = './platforms/android/app/build.gradle';
+const gradleFilename = './platforms/android/build.gradle';
 const buildContent = fs.readFileSync(gradleFilename).toString();
 const APPLY_KOTLIN_ANDROID = '\napply plugin: "kotlin-android"\n';
 const APPLY_KOTLIN_ANDROID_EXTENSIONS = '\napply plugin: "kotlin-android-extensions"\n';
-const KOTLIN_VERSION = '\next.kotlin_version = "1.2.31"\n';
+const KOTLIN_VERSION = '\next.kotlin_version = "1.2.41"\n';
 const KOTLIN_GRADLE_PLUGIN = '\nclasspath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"\n';
 let rewrite = buildContent;
 // TODO:Not Exist Kotlin-Android
 if (!buildContent.match(/apply\s+plugin(:|\s+:)(\s+|)(['"])kotlin-android(['"])/g)) appendContent(/com.android.application['"]/g, APPLY_KOTLIN_ANDROID);
 // TODO:Not Exist Kotlin-Android-Extensions
-if (!buildContent.match(/apply\s+plugin(:|\s+:)(\s+|)(['"])kotlin-android-extansions(['"])/g)) appendContent(/com.android.application['"]/g, APPLY_KOTLIN_ANDROID_EXTENSIONS);
+if (!buildContent.match(/apply\s+plugin(:|\s+:)(\s+|)(['"])kotlin-android-extensions(['"])/g)) appendContent(/com.android.application['"]/g, APPLY_KOTLIN_ANDROID_EXTENSIONS);
 // TODO:Not Exist Kotlin-Version
 if (!buildContent.match(/ext.kotlin_version/g)) appendContent(/buildscript(\s+|)\{\s+/g, KOTLIN_VERSION);
 // TODO:Not Exist Kotlin-Gradle-Plugin
